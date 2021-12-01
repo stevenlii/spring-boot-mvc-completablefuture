656c1027c29e8b6943ec5c9742f760b822ded21d
lizhiqiang09 <lizhiqiang09@meicai.cn>
2021-12-01 11:00:31 +0800

    completable async2 not actived

--- src/main/java/net/andreaskluth/CompletableFutureContoller.java
+++ src/main/java/net/andreaskluth/CompletableFutureContoller.java
@@ -13,30 +13,35 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class CompletableFutureContoller {
 
-  private static final Log LOG = LogFactory.getLog(CompletableFutureContoller.class);
-  private SomeService someService;
+	private static final Log LOG = LogFactory.getLog(CompletableFutureContoller.class);
+	private SomeService someService;
 
-  @Autowired
-  public CompletableFutureContoller(SomeService someService) {
-    this.someService = someService;
-  }
+	@Autowired
+	public CompletableFutureContoller(SomeService someService) {
+		this.someService = someService;
+	}
 
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public ModelAndView index() {
-    return new ModelAndView("index", "message", "bye bye sync world");
-  }
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index() {
+		return new ModelAndView("index", "message", "bye bye sync world");
+	}
 
-  @RequestMapping(value = "/sync", method = RequestMethod.GET)
-  public ModelAndView sync() {
-    return new ModelAndView("message", "message", "bye bye sync world");
-  }
+	@RequestMapping(value = "/sync", method = RequestMethod.GET)
+	public ModelAndView sync() {
+		return new ModelAndView("message", "message", "bye bye sync world");
+	}
 
-  @RequestMapping(value = "/async", method = RequestMethod.GET)
-  public CompletableFuture<ModelAndView> indexAsync() {
-    LOG.info("Servlet Thread Id = '" + Thread.currentThread().getName() + "'.");
-    
-    return someService.getMessage()
-      .thenApply(msg -> new ModelAndView("message", "message", msg));
-  }
+	@RequestMapping(value = "/async", method = RequestMethod.GET)
+	public CompletableFuture<ModelAndView> indexAsync() {
+		LOG.info("Servlet Thread Id = '" + Thread.currentThread().getName() + "'.");
+
+		return someService.getMessage().thenApply(msg -> new ModelAndView("message", "message", msg));
+	}
+
+	@RequestMapping(value = "/async2", method = RequestMethod.GET)
+	public ModelAndView indexAsync2() {
+		LOG.info("Servlet Thread Id = '" + Thread.currentThread().getName() + "'.");
+		return new ModelAndView("message", "message", someService.getMessage2());
+	}
 
 }
--- src/main/java/net/andreaskluth/SomeService.java
+++ src/main/java/net/andreaskluth/SomeService.java
@@ -8,20 +8,46 @@ import org.springframework.stereotype.Component;
 @Component
 public class SomeService {
 
-  @AsyncTimed
-  public CompletableFuture<String> getMessage() {
-    return CompletableFuture.supplyAsync(() -> {
-      heavyWork();
-      return "hello async world";
-    }, ForkJoinPool.commonPool());
+	@AsyncTimed
+	public CompletableFuture<String> getMessage() {
+
+		return CompletableFuture.supplyAsync(() -> {
+			heavyWork();
+			return "hello async world";
+		}, ForkJoinPool.commonPool());
+	}
+	private void heavyWork() {
+		try {
+			Thread.sleep(42);
+		} catch (InterruptedException e) {
+			Thread.interrupted();
+		}
+	}
+	public String getMessage2() {
+		CompletableFuture<String> messResult =  CompletableFuture.supplyAsync(() -> {
+			return heavyWork2();
+		}, ForkJoinPool.commonPool());
+		return messResult.join();
+	}
+	public String heavyWork2() {
+	  try {
+		  Thread.sleep(42);
+		  return getMessage3().join();
+	  }
+	  catch (InterruptedException e) {
+		  Thread.interrupted();
+	  }
+	return null;
   }
 
-  private void heavyWork() {
-    try {
-      Thread.sleep(42);
-    }
-    catch (InterruptedException e) {
-      Thread.interrupted();
-    }
-  }
+	private String convertToB(String a) {
+		return "hello async2 world";
+	}
+	@AsyncTimed
+	public CompletableFuture<String> getMessage3() {
+		return CompletableFuture.supplyAsync(() -> {
+			heavyWork();
+			return "hello async world";
+		}, ForkJoinPool.commonPool()).thenApply(a -> convertToB(a));
+	}
 }
